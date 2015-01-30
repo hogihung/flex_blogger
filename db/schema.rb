a2f4e84bc42a8f156d72604088621b78138bd487
John Hogarty <hogihung@gmail.com>
2015-01-29 20:36:26 -0500

    Migrate new db changes from posts work.  Fix link to categories I broke in previous push.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150127023957) do
+ActiveRecord::Schema.define(version: 20150130005938) do
 
   create_table "categories", force: true do |t|
     t.string   "description"
@@ -42,5 +42,17 @@ ActiveRecord::Schema.define(version: 20150127023957) do
   end
 
   add_index "posts", ["slug"], name: "index_posts_on_slug", unique: true
+
+  create_table "users", force: true do |t|
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
+    t.string   "email",                          null: false
+    t.string   "encrypted_password", limit: 128, null: false
+    t.string   "confirmation_token", limit: 128
+    t.string   "remember_token",     limit: 128, null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email"
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token"
 
 end
