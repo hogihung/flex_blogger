cc25184adea96a08f38725e91b4813194cc95ad6
Joshua Plicque <plicjo@gmail.com>
2015-01-29 20:16:15 -0500

    Scaffold posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,15 +1,26 @@
 class Post < ActiveRecord::Base
+  default_scope -> { order('created_at DESC') }
+
+  extend FriendlyId
+  friendly_id :title, use: [:slugged, :history]
 
   validates :title, presence: true
   validates :title, length: { maximum: 50 }
 
   validates :body, presence: true
+  validates :slug, presence: true
 
-  validates :status, presence: true, inclusion: { in: :acceptable_status}
-
+  # validates :status, presence: true, inclusion: { in: :acceptable_status }
 
   private
+
   def acceptable_status
     %w(draft published archived retired)
   end
+
+  # Friendly ID method for generating slugs
+  def should_generate_new_friendly_id?
+    title_changed?
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  resources :posts
   resources :categories
-	resources :home, :only => [:index]
-	resources :contact, :only => [:index]
-  resources :about, :only => [:index]
+	resources :home,    only: :index
+	resources :contact, only: :index
+  resources :about,   only: :index
 
   root to: 'categories#new'
 end
