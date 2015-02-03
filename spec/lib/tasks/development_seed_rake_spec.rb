dfe6cfa7d53bc3c71261fc5036ec535c40851364
John Hogarty <hogihung@gmail.com>
2015-02-02 19:31:54 -0500

    Update dev seed rake spec to replace single quotes with double quotes.

--- spec/lib/tasks/development_seed_rake_spec.rb
+++ spec/lib/tasks/development_seed_rake_spec.rb
@@ -1,18 +1,18 @@
-require 'rails_helper'
-require 'rake'
+require "rails_helper"
+require "rake"
 
 describe "dev namespace rake task", type: :rake do
 
   describe "dev:prime" do
 
     before :all do
-      Rake.application.rake_require 'tasks/development_seed'
+      Rake.application.rake_require "tasks/development_seed"
       Rake::Task.define_task(:environment)
-      Rake::Task.define_task('db:setup')
+      Rake::Task.define_task("db:setup")
     end
 
     let :run_rake_task do
-      Rake::Task['dev:prime'].reenable
+      Rake::Task["dev:prime"].reenable
       Rake.application.invoke_task "dev:prime"
     end
 
