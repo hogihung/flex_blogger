182b26f2e6575bab37ff4169df3215311ba99348
John Hogarty <hogihung@gmail.com>
2015-02-02 19:57:40 -0500

    Fixing space issue caused by tab and missing newline.

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -2,10 +2,10 @@ require "rails_helper"
 
 feature "Viewing Home Page" do
   scenario "a visitor clicks on the blog link" do
-   	visit root_path
+    visit root_path
 
-		click_link "Blog"
+    click_link "Blog"
 
-		expect(page).to have_content "My Cool Blog Name Here"
-	end
+    expect(page).to have_content "My Cool Blog Name Here"
+  end
 end
