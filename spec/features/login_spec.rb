5d434524827509e443377e35cf9337b777a604a2
John Hogarty <hogihung@gmail.com>
2015-02-02 19:30:29 -0500

    Update spec files to convert single quotes with double quotes.  Update to rails_helper file so it includes the spec support path.

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -1,23 +1,21 @@
-require 'rails_helper'
-require 'support/sign_in_helper'
-require 'support/sign_out_helper'
+require "rails_helper"
 
-feature 'User logs into site' do
+feature "User logs into site" do
 	background do
 		@user = FactoryGirl.create(:user)
 	end
 
-	scenario 'user logs in' do
+	scenario "user logs in" do
 		sign_in(@user)
 
-		expect(page).to have_link 'Admin'
+		expect(page).to have_link "Admin"
 	end
 
-	scenario 'user logs out' do
+	scenario "user logs out" do
 		sign_in(@user)
 
 		sign_out
 
-		expect(page).to have_link 'Sign In'
+		expect(page).to have_link "Sign In"
 	end
 end
--- spec/features/posts_spec.rb
+++ spec/features/posts_spec.rb
@@ -1,33 +1,33 @@
-require 'rails_helper'
+require "rails_helper"
 
-feature 'Managing Posts' do
-  scenario 'Create a new post' do
+feature "Managing Posts" do
+  scenario "Create a new post" do
     visit new_post_path
 
-    fill_in 'post_title', with: 'Title'
-    fill_in 'post_body',  with: 'Body'
-    select 'draft', from: 'post_status'
-    click_button 'Create Post'
+    fill_in "post_title", with: "Title"
+    fill_in "post_body",  with: "Body"
+    select "draft", from: "post_status"
+    click_button "Create Post"
 
-    expect(page).to have_content 'Successfully created'
+    expect(page).to have_content "Successfully created"
   end
 
-  scenario 'Edit a post' do
+  scenario "Edit a post" do
     @post = FactoryGirl.create(:post)
     visit edit_post_path(@post)
 
-    fill_in 'post_body', with: 'Bodies'
-    click_button 'Update Post'
+    fill_in "post_body", with: "Bodies"
+    click_button "Update Post"
 
-    expect(page).to have_content 'Successfully updated'
+    expect(page).to have_content "Successfully updated"
   end
 
-  scenario 'Delete a post' do
+  scenario "Delete a post" do
     @post = FactoryGirl.create(:post)
     visit posts_path
 
-    click_link 'Delete'
+    click_link "Delete"
 
-    expect(page).to have_content 'Successfully deleted'
+    expect(page).to have_content "Successfully deleted"
   end
 end
