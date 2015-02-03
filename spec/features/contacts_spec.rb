aa195d71501a84fb3b0804fc9897b146592a908f
John Hogarty <hogihung@gmail.com>
2015-02-02 19:59:31 -0500

    Fix spacing issue caused by tabs in contacts spec.

--- spec/features/contacts_spec.rb
+++ spec/features/contacts_spec.rb
@@ -2,10 +2,10 @@ require "rails_helper"
 
 feature "Viewing Contact Page" do
   scenario "a visitor clicks on contacts link" do
-   	visit root_path
+    visit root_path
 
-		click_link "Contact"
+    click_link "Contact"
 
-		expect(page).to have_content "Contact Us"
-	end
+    expect(page).to have_content "Contact Us"
+  end
 end
