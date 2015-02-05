1293a990ccb36af7794c3f6bf9649a79046dac73
John Hogarty <hogihung@gmail.com>
2015-02-04 22:03:41 -0500

    Update visitor home spec to include test for admin menu link

--- spec/features/visitor/home_spec.rb
+++ spec/features/visitor/home_spec.rb
@@ -1,11 +1,15 @@
 require "rails_helper"
 
 feature "Viewing Home Page" do
-  scenario "a visitor clicks on the blog link" do
+  scenario "a visitor visits the home page (root url)" do
     visit root_path
 
-    click_link "Blog"
-
     expect(page).to have_content "My Cool Blog Name Here"
+  end
+
+  scenario "a visitor should not see admin menu" do
+    visit root_path
+
+    expect(page).to_not have_link "Admin"
   end
 end
