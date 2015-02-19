4c6e87b050186e6522edf93656d6ea06d903b76d
John Hogarty <hogihung@gmail.com>
2015-02-18 20:26:38 -0500

    Round out admin can administrate contributors.

--- spec/features/user/contributor_admin_spec.rb
+++ spec/features/user/contributor_admin_spec.rb
@@ -35,12 +35,27 @@ feature "Managing user resource" do
 
     manage_contributor
 
-    find(:xpath, "//tr[contains(.,'jack')]/td/a", :text => 'Edit').click
+    find(:xpath, "//tr[contains(.,'jack')]/td/a", :text => "Edit").click
     check("Admin")
     click_button "Update User"
 
     expect(page).to have_content "Update successfull!"
     expect(page).to have_xpath("//tr[contains(.,'jack')][contains(.,'true')]")
+  end
+
+  scenario "Admin can change the password for another user." do
+    contrib_one
+
+    manage_contributor
+
+    find(:xpath, "//tr[contains(.,'jack')]/td/a", :text => "Edit").click
+    fill_in "Password", with: "nopwdReqd"
+    click_button "Update User"
+
+    expect(page).to have_content "Update successfull!"
+
+    sign_out
+    sign_in_user_with_password_changed
   end
 
   scenario "A non-admin should not be able to access user admin resource." do
@@ -60,14 +75,14 @@ feature "Managing user resource" do
     expect(page).to have_content "Not Authorized"
   end
 
-  # scenario "A non-admin should not be able to edit a user admin resource directly." do
-  #   user
-  #   sign_in(contrib_two)
+   scenario "A non-admin should not be able to edit a user admin resource directly." do
+     user
+     sign_in(contrib_two)
 
-  #   visit "/users/1/edit"
+     visit "/users/1/edit"
 
-  #   expect(page).to have_content "Not Authorized"
-  # end
+     expect(page).to have_content "Not Authorized"
+   end
 
   scenario "A visitor should not be able to access user admin resource directly. " do
     visit "/users"
@@ -81,4 +96,14 @@ feature "Managing user resource" do
     click_link "Admin"
     click_link "Contributors"
   end
+
+  def sign_in_user_with_password_changed
+    visit sign_in_path
+    fill_in "session_email", with: "jack@example.com"
+    fill_in "session_password", with: "nopwdReqd"
+    click_button "Sign in"
+
+    expect(page).to have_link "Admin"
+  end
+
 end
