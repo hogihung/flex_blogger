12e53e30ad0bfe57e31aeadf16d7b7c5e802bac4
John Hogarty <hogihung@gmail.com>
2015-02-19 18:51:46 -0500

    Update for hound reporting that I should use new syntax for hashes.

--- spec/features/user/contributor_admin_spec.rb
+++ spec/features/user/contributor_admin_spec.rb
@@ -35,7 +35,7 @@ feature "Managing user resource" do
 
     manage_contributor
 
-    find(:xpath, "//tr[contains(.,'jack')]/td/a", :text => "Edit").click
+    find(:xpath, "//tr[contains(.,'jack')]/td/a", text: "Edit").click
     check("Admin")
     click_button "Update User"
 
@@ -48,7 +48,7 @@ feature "Managing user resource" do
 
     manage_contributor
 
-    find(:xpath, "//tr[contains(.,'jack')]/td/a", :text => "Edit").click
+    find(:xpath, "//tr[contains(.,'jack')]/td/a", text: "Edit").click
     fill_in "Password", with: "nopwdReqd"
     click_button "Update User"
 
@@ -62,7 +62,7 @@ feature "Managing user resource" do
     contrib_one
 
     manage_contributor
-    find(:xpath, "//tr[contains(.,'jack')]/td/a", :text => "Delete").click
+    find(:xpath, "//tr[contains(.,'jack')]/td/a", text: "Delete").click
 
     expect(page).to have_content "Contributor removed successfully."
   end
