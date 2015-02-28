8bcb8e69dfe7d9b13ada7cc116aea3c457c0e96e
John Hogarty <hogihung@gmail.com>
2015-02-28 18:01:19 -0500

    Round out the About page.

--- spec/features/user/about_spec.rb
+++ spec/features/user/about_spec.rb
@@ -6,6 +6,9 @@ feature "Viewing About Page" do
 
     click_link "About"
 
-    expect(page).to have_content "The About Page"
+    expect(page).to have_content "About Me"
+    expect(page).to have_content "What is this site all about?"
+    expect(page).to have_content "details that a new person is"
+    expect(page).to have_content "One of my goals is to pass on"
   end
 end
