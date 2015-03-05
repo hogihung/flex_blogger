df8640eebebca22f903b17d5611b80bdc3e876c2
John Hogarty <hogihung@gmail.com>
2015-03-04 20:31:12 -0500

    User can change their own display name.

--- spec/features/user/user_edit_spec.rb
+++ spec/features/user/user_edit_spec.rb
@@ -8,14 +8,32 @@ feature "Edit user settings," do
   given(:post)         { create :post }
 
   scenario "A user can change their own password" do
+    user_updates_themself
+
+    fill_in "Password", with: "a_newPassword"
+    click_on "Update User"
+
+    expected_result_on_update
+  end
+
+  scenario "A user can change their own display name" do
+    user_updates_themself
+
+    fill_in "Display name", with: "OdoRuff"
+    click_on "Update User"
+
+    expected_result_on_update
+  end
+
+  def user_updates_themself
     post
     sign_in(contrib_one)
 
     click_link "Manage"
     click_link contrib_one.display_name
-    fill_in "Password", with: "a_newPassword"
-    click_on "Update User"
+  end
 
+  def expected_result_on_update
     expect(page).to have_content "Update successful!"
     expect(page).to have_content "Your Awesome Blog Name Goes Here"
     expect(page).to have_content "Blog Post Title Here"
