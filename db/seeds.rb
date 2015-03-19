aa2a09bda74181e6a07e0930b550b9fe59c95a94
John Hogarty <hogihung@gmail.com>
2015-03-19 14:08:02 -0400

    Update comments of seeds file and create three default post categories.'

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,16 @@
 #        Note: If seeding for production, you may need to use:
 #              rake RAILS_ENV=production db:seed
 #
-User.create(email: "admin_user@example.com", password: "rob0ts84", display_name: "Sog", admin: true)
-User.create(email: "mod_one_user@example.com", password: "mypassw0rd", display_name: "Moddyun", admin: false)
-User.create(email: "mod_two_user@example.com", password: "mydogsname", display_name: "Moddydu", admin: false)
+# ------------------------------------------------------------------------------
+# Create an admin user and two contributors
+User.create(email: "admin_user@example.com", password: "rob0ts84",
+            display_name: "Sog", admin: true)
+User.create(email: "mod_one_user@example.com", password: "mypassw0rd",
+            display_name: "Moddyun", admin: false)
+User.create(email: "mod_two_user@example.com", password: "mydogsname",
+            display_name: "Moddydu", admin: false)
+# Create three default blog post categories - you can add more here or via the
+# web gui after you login as an Admin
+Category.create(description: "Programming")
+Category.create(description: "DevOps")
+Category.create(description: "Social")
