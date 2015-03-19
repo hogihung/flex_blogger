48662a52938d7828690d4e06b443883eb86918c4
John Hogarty <hogihung@gmail.com>
2015-03-19 13:45:03 -0400

    Clean up comment section and remove extra spacing in seeds file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,9 +4,8 @@
 #        rake db:seed
 #
 #        Note: If seeding for production, you may need to use:
-#        rake RAILS_ENV=production db:seed
+#              rake RAILS_ENV=production db:seed
 #
 User.create(email: "admin_user@example.com", password: "rob0ts84", display_name: "Sog", admin: true)
 User.create(email: "mod_one_user@example.com", password: "mypassw0rd", display_name: "Moddyun", admin: false)
 User.create(email: "mod_two_user@example.com", password: "mydogsname", display_name: "Moddydu", admin: false)
-
