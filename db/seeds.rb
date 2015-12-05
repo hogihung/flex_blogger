007ed73d77252790ac92936e48ccfa087d7f5c61
John Hogarty <hogihung@gmail.com>
2015-12-05 10:10:32 -0500

    Minor adjustment to seeds file so we do not have duplicate user name.

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,7 @@ User.create(email: "admin_user@example.com", password: "rob0ts84",
 User.create(email: "mod_one_user@example.com", password: "mypassw0rd",
             display_name: "Moddyun", admin: false)
 User.create(email: "mod_two_user@example.com", password: "mydogsname",
-            display_name: "Moddydu", admin: false)
+            display_name: "Modydun", admin: false)
 # Create three default blog post categories - you can add more here or via the
 # web gui after you login as an Admin
 Category.create(description: "Programming")
