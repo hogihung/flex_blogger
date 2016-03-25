0d0c5c353f32397a62e437bbe68ad7d70d9d3a72
John Hogarty <hogihung@gmail.com>
2016-03-25 09:08:21 -0400

    Configuration changes to support capybara screenshot.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,6 +2,7 @@
 require 'spec_helper'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
+require 'capybara-screenshot/rspec'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
---
+++ spec/support/capybara.rb
@@ -0,0 +1,56 @@
+require 'capybara'
+require 'capybara/rspec'
+require 'selenium-webdriver'
+require 'capybara/poltergeist'
+
+if ENV['SELENIUM']
+  Capybara.default_max_wait_time = 10
+  Capybara.default_driver = :selenium
+  Capybara.register_driver :selenium do |app|
+    prefs = {
+      'download' => {
+        'default_directory' => DownloadHelpers.path.to_s,
+        'directory_upgrade' => true,
+        'extensions_to_open' => ''
+      }
+    }
+    caps = Selenium::WebDriver::Remote::Capabilities.chrome
+    caps['chromeOptions'] = {'prefs' => prefs,
+                              'args' => ['disable-desktop-notifications']
+                            }
+    Capybara::Selenium::Driver.new(app,
+                                  :browser => :chrome,
+                                  :desired_capabilities => caps,
+                                  :args => ["--window-size=1920,1080"]
+    )
+  end
+
+  Capybara.run_server = false
+  Capybara.app_host = 'http://localhost:3000'
+
+  # Check to see if the server is running (for Selenium tests only)
+  if Capybara.default_driver == :selenium && !Capybara.run_server
+    begin
+      open(Capybara.app_host)
+    rescue
+      puts "\nCannot connect to server #{Capybara.app_host}\n\n"
+      exit
+    end
+  end
+
+else
+  # Poltergeist -- the default
+  Capybara.register_driver :poltergeist do |app|
+    Capybara::Poltergeist::Driver.new(
+      app,
+      {
+        timeout: 120,
+        debug: false, # change this to true to troubleshoot
+        window_size: [1920, 1040] # this can affect dynamic layout
+      }
+    )
+  end
+  Capybara.javascript_driver = :poltergeist
+  Capybara.default_driver = :poltergeist
+
+end
