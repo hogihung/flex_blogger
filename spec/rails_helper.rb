828e77d80f8ebcdd7430d96f84bf3fc2fb98ee56
John Hogarty <hogihung@gmail.com>
2016-03-25 14:49:08 -0400

    Add configuration to support newer version of shoulda matchers.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -36,3 +36,16 @@ RSpec.configure do |config|
   config.run_all_when_everything_filtered = true
   config.filter_run_including focus: true
 end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+
+    # Choose one or more libraries:
+    with.library :active_record
+    with.library :active_model
+    #with.library :action_controller
+    # Or, choose the following (which implies all of the above):
+    #with.library :rails
+  end
+end
